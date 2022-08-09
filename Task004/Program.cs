5903c0107fb2bbbc1dba2e476550f0c2056a1515
Maratius <maratius.ru@gmail.com>
2022-08-10 02:13:26 +0300

    task004

---
+++ Task004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число больше единицы");
+int N = Convert.ToInt32(Console.ReadLine());
+int x = 2;
+if (N > 1) 
+{
+    while (x <= N)
+    {
+        if (x % 2 == 0)
+            Console.WriteLine(x);
+        x++;
+    }
+} 
+else
+{
+    Console.WriteLine("Число меньше единицы");
+}
